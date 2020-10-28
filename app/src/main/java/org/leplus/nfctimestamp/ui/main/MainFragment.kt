a5d5879fde73a73bd6ae14beafb0370faaad4f3d
Thomas Leplus <thomas.leplus@gmail.com>
2020-10-27 21:12:44 -0700

    Organizing code

--- app/src/main/java/org/leplus/nfctimestamp/ui/main/MainFragment.kt
+++ app/src/main/java/org/leplus/nfctimestamp/ui/main/MainFragment.kt
@@ -1,10 +1,11 @@
-package org.leplus.nfctimestamp
+package org.leplus.nfctimestamp.ui.main
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import org.leplus.nfctimestamp.R
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
