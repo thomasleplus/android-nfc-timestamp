a9197fee607650b4a775fa404667bbdc02cd5e27
Thomas Leplus <thomas.leplus@gmail.com>
2020-10-27 18:12:12 -0700

    Renaming first fragment

--- app/src/main/java/org/leplus/nfctimestamp/MainFragment.kt
+++ app/src/main/java/org/leplus/nfctimestamp/MainFragment.kt
@@ -9,14 +9,14 @@ import android.view.ViewGroup
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class MainFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false)
+        return inflater.inflate(R.layout.fragment_main, container, false)
     }
 
 }
