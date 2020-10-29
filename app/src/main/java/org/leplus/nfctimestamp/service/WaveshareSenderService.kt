7fea4cba698347f7be6dd07fc8fa0d74aad7bb37
Thomas Leplus <thomasleplus@users.noreply.github.com>
2020-10-28 18:36:54 -0700

    Adding SenderService interface

--- app/src/main/java/org/leplus/nfctimestamp/service/WaveshareSenderService.kt
+++ app/src/main/java/org/leplus/nfctimestamp/service/WaveshareSenderService.kt
@@ -2,7 +2,7 @@ package org.leplus.nfctimestamp.service
 
 import waveshare.feng.nfctag.activity.WaveshareSenderAdapter
 
-class WaveshareSenderService {
+class WaveshareSenderService : SenderService {
 
     private var adapter = WaveshareSenderAdapter()
 
